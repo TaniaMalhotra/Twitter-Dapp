4f7d6679de7cb7c04ccaf409fb526023b8fa7fa5
TaniaMalhotra <malhotratanya847@gmail.com>
2023-11-24 19:21:44 +0530

    store and get tweets

--- contracts/Twitter.sol
+++ contracts/Twitter.sol
@@ -1,8 +1,34 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.6.12 <0.9.0;
 
+//make a twitter contract and add likes functionailty
+
+
+
 contract Twitter {
-  function print() public pure returns (string memory) {
-    return "Hello World!";
-  }
+  
+  struct tweets{
+  string content;
+  uint256 timestamp;
+  address author;
+  uint id;
+}
+mapping(address => tweets[]) tweet;
+
+//store tweets
+function storeTweet(string memory content) public{
+  tweets memory onetweet = tweets({
+    content:content,
+    timestamp: block.timestamp,
+    author: msg.sender,
+    id: tweet[msg.sender].length
+  });
+  tweet[msg.sender].push(onetweet);
+}
+
+//get a particular tweet
+function getTweet(address author, uint id) public view returns(string memory){
+  return tweet[author][id].content;
+}
+  
 }
